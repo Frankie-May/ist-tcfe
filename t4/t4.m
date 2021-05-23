af6d855521bf335fd599aab0edd8a0255c2b7431
dmbsousa <80052731+dmbsousa@users.noreply.github.com>
2021-05-23 15:50:53 +0100

    Modified t4.m and t4.net
    Removed a line from t4.net and added frequency analysis for gain in t4.m

--- t4/t4.m
+++ t4/t4.m
@@ -10,6 +10,11 @@ RB2=20000
 VBEON=0.7
 VCC=12
 RS=100
+Ci = 1e-3
+Cb = 1e-3
+Co = 1e-6
+RL = 8
+Vin = 10e-3
 
 RB=1/(1/RB1+1/RB2)
 VEQ=RB2/(RB1+RB2)*VCC
@@ -74,3 +79,53 @@ AV = (gB+gm2/gpi2*gB)/(gB+ge2+go2+gm2/gpi2*gB)*AV1
 AV_DB = 20*log10(abs(AV))
 ZI=ZI1
 ZO=1/(go2+gm2/gpi2*gB+ge2+gB)
+
+
+%frequency response
+RE1=100;
+rpi2 = 1/gpi2;
+ro2 = 1/go2;
+Af = [];
+Af_db = [];
+i=1;
+
+for f=1:0.1:8
+	w = 2*pi*power(10,f);
+	Z_Ci = (1/(j*w*Ci));
+	Z_Cb = (1/(j*w*Cb));
+	Z_Co = (1/(j*w*Co));
+	ZA = RS+Z_Ci;
+	ZB = 1/(1/RE1 + 1/Z_Cb);
+	ZC = (1/((1/RE2) + (1/(Z_Co + RL))));
+
+	T = [-(RB + ZA) , RB , 0 , 0 , 0 , 0 , 0; %tafixe
+		RB , -(ZB + rpi1 + RB) , 0 , ZB , 0 , 0 , 0; %tafixe
+		0 , gm1*rpi1 , 1 , 0 , 0 , 0 , 0; %tafixe
+		0 , ZB , ro1 , -(ZB + ro1 + RC1) , RC1 , 0 , 0; %tafixe
+		0 , 0 , 0 , RC1 , -(ZC + rpi2 +RC1) , 0 , ZC; %tafixe
+		0 , 0 , 0 , 0 , gm2*rpi2 , 1 , 0; %tafixe
+		0 , 0 , 0 , 0 , ZC , ro2 , -(ZC + ro2)] %tafixe
+	U = [Vin;
+		0;
+		0;
+		0;
+		0;
+		0;
+		0]
+	H = T\U;
+
+	vo = (ZC * (H(7) - H(5)));
+	Af(i) = ((RL/(RL + Z_Co))* vo)/Vin;
+	Af_db(i) = 20*(log10(abs(Af(i))));
+	i++;
+endfor;
+H
+f=1:0.1:8;
+
+gain = figure();
+plot(f , Af_db , "r");
+title("Gain in decibels");
+legend("Gain");
+xlabel ("log_{10} (f) [Hz]");
+ylabel ("Gain [dB]");
+print (gain, "gain.eps", "-depsc");
