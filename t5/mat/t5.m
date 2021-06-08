04b07f5fc3169a64498d3e503136e211d1152bb0
Frankie-May <Francisco.May@tecnico.ulisboa.pt>
2021-06-08 15:38:38 +0100

    Error in simulation!

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -1,12 +1,3 @@
-%Variables
-
-%Ci = 0.3u;
-%Ri = 1000;
-%R4 = 1k;
-%R3 = 10k;
-%Ro = 100;
-%Co = 8E-7;
-
 %Gain Stage
 
 Vcc=5.0;
@@ -62,19 +53,19 @@ omega_0 = sqrt(omega_L*omega_H);
 
 fileoutp = fopen("outputstg.tex" , "w");
 
-fprintf(fileoutp , "The Gain of the Gain Stage && %f \\\\ \\hline \n" , (1+(R2)/(R1)));
-fprintf(fileoutp , "$\\omega_L$ [rad/s] && %f \\\\ \\hline \n" , omega_L);
-fprintf(fileoutp , "$\\omega_H$ [rad/s] && %f \\\\ \\hline \n" , omega_H);
-fprintf(fileoutp , "$\\omega_0$ [rad/s] && %f \\\\ \\hline \n" , omega_0);
-fprintf(fileoutp , "Gain ($\\omega_0$) [dB] && %f \\\\ \\hline \n" , 20*log10 (abs(transf (omega_0*j))));
-fprintf(fileoutp , "Gain ($2000\\pi$) [dB] && %f \\\\ \\hline \n" , 20*log10 (abs(transf (2000*pi()*j))));
-fprintf(fileoutp , "Imput Impedance $Z_in$ [Ohm] && %f \\\\ \\hline \n" , (1)/(j*omega_0*Clcut)+Rlcut);
-fprintf(fileoutp , "Output Impedance $Z_out$ [Ohm] && %f \\\\ \\hline \n" , (1)/(j*omega_0*Chcut+(1)/(Rhcut)));
+fprintf(fileoutp , "The Gain of the Gain Stage & %f \\\\ \\hline \n" , (1+(R2)/(R1)));
+fprintf(fileoutp , "$\\omega_L$ [rad/s] & %f \\\\ \\hline \n" , omega_L);
+fprintf(fileoutp , "$\\omega_H$ [rad/s] & %f \\\\ \\hline \n" , omega_H);
+fprintf(fileoutp , "$\\omega_0$ [rad/s] & %f \\\\ \\hline \n" , omega_0);
+fprintf(fileoutp , "Gain ($\\omega_0$) [dB] & %f \\\\ \\hline \n" , 20*log10 (abs(transf (omega_0*j))));
+fprintf(fileoutp , "Gain ($2000\\pi$) [dB] & %f \\\\ \\hline \n" , 20*log10 (abs(transf (2000*pi()*j))));
+fprintf(fileoutp , "Imput Impedance $Z_{in}$ [Ohm] & %f \\\\ \\hline \n" , (1)/(j*omega_0*Clcut)+Rlcut);
+fprintf(fileoutp , "Output Impedance $Z_{out}$ [Ohm] & %f \\\\ \\hline \n" , (1)/(j*omega_0*Chcut+(1)/(Rhcut)));
 fclose (fileoutp);
 
 while (i <= 100)
 	Af(i) = 20*log10 (abs(transf (w(i)*j)));
-	PHf(i) = arg (transf (w(i)*j))*(180)/(pi);
+	PHf(i) = arg (transf (w(i)*j));
 	%printf("w(%d) = %f\n", i, w(i));
 	%printf("Af(%d) = %f\n", i, Af(i));
 	%printf("\n");
@@ -95,11 +86,11 @@ print (gain, "gain.eps", "-depsc");
 
 phase = figure();
 semilogx(w , PHf , "c");
-title("Phase in degrees");
+title("Phase in radians");
 legend("Phase");
 grid on;
 xlabel ("log_{10} (w) [rad/s]");
-ylabel ("Phase [degrees]");
+ylabel ("Phase [radians]");
 print (phase, "phase.eps", "-depsc");
 
 t=0:0.05:10;
